8de5a6e9835ba2c840e63c5695a7a4ef944c656f
skadluba <90658540+skadluba@users.noreply.github.com>
2021-10-13 16:50:05 -0300

    Update IOwnerRepository.cs

--- Contracts/IOwnerRepository.cs
+++ Contracts/IOwnerRepository.cs
@@ -7,11 +7,13 @@ namespace Contracts
 {
     public interface IOwnerRepository : IRepositoryBase<Owner>
     {
+
         PagedList<Owner> GetOwners(OwnerParameters ownerParameters);
         Owner GetOwnerById(Guid ownerId);
         Owner GetOwnerWithDetails(Guid ownerId);
         void CreateOwner(Owner owner);
         void UpdateOwner(Owner owner);
         void DeleteOwner(Owner owner);
+
     }
 }
